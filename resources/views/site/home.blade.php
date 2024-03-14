8a6e75b48f8a88fc7a39415da4953d35b63492c5
pattiesanchez <pattuhwang42@gmail.com>
2024-03-14 20:25:30 -0300

    atualizando

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -7,11 +7,10 @@
         <div class="titulo-pagina-2">
             <h1>Home</h1>
         </div>
-
         <div class="sobre-nos">
-            <div style="width:100%; margin-left: auto; margin-right: auto;">
+            <div style="width:100%; margin-left: auto; margin-right: auto; padding-bottom: 50px">
                 <h1>Estoque Inteligente: <br>Simplifica o controle de estoque <br>e maximiza seus resultados!</h1>
-                <br>
+                <br>                <hr>
                 <h2>Quem somos?</h2>
                 <span id="text-span">Seja bem-vindo à Estoque Inteligente, onde a busca pela excelência e inovação se encontra com o compromisso inflexível com a eficiência. 
                     Nascemos com a visão ousada de redefinir os padrões da indústria de distribuição, e desde então, consolidamos nossa posição como uma empresa 
@@ -19,15 +18,19 @@
                     Por isso, estamos sempre disponíveis para oferecer suporte personalizado e assistência especializada, garantindo que cada cliente possa aproveitar 
                     ao máximo nossa tecnologia e alcançar seus objetivos de negócios.
                 </span>
-                <h2>Quem somos?</h2>
-                <span id="text-span">Seja bem-vindo à Estoque Inteligente, onde a busca pela excelência e inovação se encontra com o compromisso inflexível com a eficiência. 
-                    Nascemos com a visão ousada de redefinir os padrões da indústria de distribuição, e desde então, consolidamos nossa posição como uma empresa 
-                    dedicada a oferecer soluções inovadoras e personalizadas.Na Estoque Inteligente, acreditamos que o sucesso de nossos clientes é o nosso sucesso. 
-                    Por isso, estamos sempre disponíveis para oferecer suporte personalizado e assistência especializada, garantindo que cada cliente possa aproveitar 
-                    ao máximo nossa tecnologia e alcançar seus objetivos de negócios.
+                <h2>Missão, Visão e Valores:</h2>
+                <span id="text-span"><b>Missão:</b><br>Facilitar a gestão de estoque para empresas de todos os portes, oferecendo soluções inteligentes e eficazes por meio de nosso 
+                    software, garantindo que os clientes possam armazenar e encontrar produtos de forma rápida, precisa e econômica.<br><br><b>Visão:</b><br>Ser reconhecida como líder de 
+                    mercado em soluções de gestão de estoque, destacando-nos pela inovação, eficiência e compromisso com a satisfação do cliente. Almejamos tornar-nos o parceiro 
+                    preferido das empresas que buscam otimizar seus processos de armazenamento e controle de estoque.<br><br><b>Valores:</b><br><b>Excelência:</b> Buscamos a excelência em 
+                    tudo o que fazemos, desde o desenvolvimento do software até o atendimento ao cliente. <br><b>Inovação:</b> Sempre em busca de novas ideias e tecnologias 
+                    para aprimorar nossas soluções e oferecer o que há de mais avançado no mercado. <br><b>Confiança:</b> Valorizamos a confiança depositada em nós pelos nossos clientes e 
+                    colaboradores, agindo sempre com transparência e integridade.<br><b>Colaboração:</b> Acreditamos no poder da colaboração e do trabalho em equipe para alcançar nossos 
+                    objetivos e superar desafios.<br><b>Compromisso com o cliente:</b> Colocamos as necessidades e expectativas dos nossos clientes no centro de todas as nossas ações, 
+                    buscando sempre superar suas expectativas e garantir sua satisfação.<br><b>Responsabilidade social e ambiental: </b> Atuamos de forma responsável em relação às questões 
+                    sociais e ambientais, buscando contribuir positivamente para a comunidade e o meio ambiente em que estamos inseridos.
                 </span>
             </div>
-           
         </div>
     </div>
 @endsection
