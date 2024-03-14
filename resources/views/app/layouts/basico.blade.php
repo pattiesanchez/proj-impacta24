0029030c9ccc6b526805bc4971dedbab623458bb
pattiesanchez <pattuhwang42@gmail.com>
2024-03-14 19:34:01 -0300

    inicio page

--- resources/views/app/layouts/basico.blade.php
+++ resources/views/app/layouts/basico.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Super Gestão - @yield('titulo')</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,12 +3,22 @@
 @section('titulo', 'Home')
 
 @section('conteudo')
-    <br><br><br><br>Home<br><br>
+   <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <h1>Home</h1>
+        </div>
 
-    Na  home terá.<br><br>
-
-    1. Quem somos<br>
-    1.1 Valores<br>
-    2. Redes sociais<br>
-    3. Contatos
+        <div class="sobre-nos">
+            <div style="width:100%; margin-left: auto; margin-right: auto;">
+                <h2>Quem somos?</h2>
+                <span id="text-span">Seja bem-vindo à Estoque Inteligente, onde a busca pela excelência e inovação se encontra com o compromisso inflexível com a eficiência. 
+                    Nascemos com a visão ousada de redefinir os padrões da indústria de distribuição, e desde então, consolidamos nossa posição como uma empresa 
+                    dedicada a oferecer soluções inovadoras e personalizadas.Na Estoque Inteligente, acreditamos que o sucesso de nossos clientes é o nosso sucesso. 
+                    Por isso, estamos sempre disponíveis para oferecer suporte personalizado e assistência especializada, garantindo que cada cliente possa aproveitar 
+                    ao máximo nossa tecnologia e alcançar seus objetivos de negócios.
+                </span>
+            </div>
+           
+        </div>
+    </div>
 @endsection
