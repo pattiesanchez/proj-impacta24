98b67bd7a72248bf807efc0a12af355d4b4f6cfc
Sanchez <pattuhwang42@gmail.com>
2024-03-27 08:43:04 -0300

    atualizando main

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,29 +24,24 @@ class LoginController extends Controller
 
     public function autenticar(Request $request) {
 
-        //regras de validação
         $regras = [
             'usuario' => 'email',
             'senha' => 'required'
         ];
 
-        //as mensagens de feedback de validação
         $feedback = [
             'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
             'senha.required' => 'O campo senha é obrigatório'
         ];
 
-        //se não passar no validate
         $request->validate($regras, $feedback);
 
-        //recuperamos os parâmetros do formulário
         $email = $request->get('usuario');
         $password = $request->get('senha');
 
         echo "Usuário: $email | Senha: $password";
         echo "<br>";
 
-        //iniciar o Model User
         $user = new User();
 
         $usuario = $user->where('email', $email)
